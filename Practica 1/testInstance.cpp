b7638fe9fd1f5bd031778f756c9cb2ac77de2938
carlosccb <carlos.cb.94@gmail.com>
2016-02-28 14:43:45 +0100

    Añadidas impresion del vector cargado y mas cosas

--- Practica 1/testInstance.cpp
+++ Practica 1/testInstance.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 #include "Instance.hpp"
 
@@ -8,11 +9,13 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-	string opt;
+	int opt;
+	int inst_num;
+	vector<problem_element> inst_v;
 
 	do {
 		cout << "\n\n";
-		cout << "What problem instance would you like to load? " << endl;
+		cout << "¿De que problema desea cargar su instancia? " << endl;
 		cout << "  1. TSP" << endl;
 		cout << "  2. KP" << endl;
 		cout << "  0. Exit" << endl;
@@ -20,19 +23,38 @@ int main(int argc, char** argv) {
 		cin >> opt;
 
 		string fileName;
-		cout << "Enter the name of the file: ";
-		cin >> fileName;
-		if(std::stoi(opt) == 1) {
-			//string fileName = "TSP/berlin52-tsp.csv";
+		if(opt != 0) {
+			cout << "Introduzca el nombre del fichero: ";
+			cin >> fileName;
+
+			cout << "¿Que numero de instancia desea cargar?: ";
+			cin >> inst_num;
+		}
+
+		bool ret = false;
+
+		if(opt == 1) {
 			fileName =  "TSP/" + fileName;
 			Instance instance(fileName, 6, 3, ' ');
-			instance.load();
-		} else if(std::stoi(opt) == 2) {
+			ret = instance.loadInstance(inst_v, inst_num);
+		} else if(opt == 2) {
 			fileName =  "KP/" + fileName;
-			Instance instance(fileName, 5, 1, ',');
-			instance.load();
+			Instance instance(fileName, 5, 1, ',', 2);
+			ret = instance.loadInstance(inst_v, inst_num);
 		}
-	} while(std::stoi(opt));
+
+		if(opt != 0) {
+			if(not ret) {
+				cerr << "El fichero no se pudo abrir.\n";
+				return -1;
+			}
+
+			cout << "\nInstancia " << inst_num << " de " << fileName << endl;
+			for(unsigned int i = 0; i < inst_v.size(); i++)
+				cout << inst_v[i].id << ": \n  w: " << inst_v[i].a << "\n  p: " << inst_v[i].b << endl;
+		}
+
+	} while(opt);
 
 	return 0;
 }
