ed0aed8696060e38bee394e822b7f1e4e2d391d3
kindarnakes <kindenene@gmail.com>
2016-03-01 20:06:22 +0100

    Update SolucionMochila.hpp

--- Practica 1/SolucionMochila.hpp
+++ Practica 1/SolucionMochila.hpp
@@ -7,8 +7,6 @@ using namespace std;
 
 namespace Metaheuristica{
 
-
-//Para hacer uso de la clase, hay que usar el constructor, sino se utiliza, el número de objetos quedará fijado a 0 para evitar problemas
 class Solucion_mochila{
 
  private:
@@ -16,24 +14,23 @@ class Solucion_mochila{
 
  public:
     //Observadores
-  inline vector<int> getSolucionMochila() const
+  inline vector<int> getSolucion() const
    {return _solucion;};
 
-  int getSolucionMochila(int n) const //Para consultar un elemento de la solución, no se utiliza la posición 0
+  int getSolucion(int n) const //Para consultar un elemento de la solución, no se utiliza la posición 0
    { if (n <= _solucion.size())
       return _solucion.at(n);
      else 
       return -1;}; 
 
    //Modificadores
-  inline void setSolucionMochila(vector<int> solucion)
+  inline void setSolucion(vector<int> solucion)
    {_solucion = solucion;};
   
-  void setSolucionMochila(int pos, int valor)
-   {if (pos <= _objetos)
+  void setSolucion(int pos, int valor)
+   {if (pos <= _solucion.size())
      _solucion[pos]=valor;}; //Para modificar un elemento de la solución, no se utiliza la posicion 0
 
-  //El número de objetos no se podrá modificar, solo se inicializa al principio
   SolucionMochila(int objetos=0){
    int i;
    
