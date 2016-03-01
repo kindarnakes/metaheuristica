3e7b1823a65721fdb3b0c738dcac075971310e8a
kindarnakes <kindenene@gmail.com>
2016-03-01 20:03:11 +0100

    Update and rename Solucion.hpp to SolucionViajante.hpp

---
+++ Practica 1/SolucionViajante.hpp
@@ -0,0 +1,44 @@
+#ifndef __SOLUCIONVIAJANTE__
+#define __SOLUCIONVIAJANTE__
+
+#include <vector>
+
+using namespace std;
+
+namespace Metaheuristica{
+
+class SolucionViajante{
+
+ private:
+  vector<int> _solucion;
+
+ public:
+    //Observadores
+  inline vector<int> getSolucionViajante() const
+   {return _solucion;};
+
+  int getSolucion(int n) const //Para consultar un elemento de la solución, no se utiliza la posición 0
+   { if (n <= _solucion.size())
+      return _solucion.at(n);
+     else 
+      return -1;}; 
+
+   //Modificadores
+  inline void setSolucion(vector<int> solucion)
+   {_solucion = solucion;};
+  
+  void setSolucion(int pos, int valor)
+   {if (pos <= _objetos)
+     _solucion[pos]=valor;}; //Para modificar un elemento de la solución, no se utiliza la posicion 0
+
+  //El número de objetos no se podrá modificar, solo se inicializa al principio
+  SolucionViajante(int objetos=0){
+   int i;
+    for (i=0; i<= objetos; i++)
+     //colocamos un elemento 0 para no utilizar la posición 0 del vector
+    _solucion.push_back(i);
+    }
+  }
+ };  
+}
+#endif
