c92373f6657843eec77525beac625f932515fb2f
carlosccb <carlos.cb.94@gmail.com>
2016-03-02 11:19:12 +0100

    Añadido main vacio con las cabeceras incluidas

---
+++ Practica 1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "lib/Random.h"
+#include "lib/Timer.h"
+
+#include "KP/SolucionMochila.hpp"
+#include "TSP/SolucionViajante.hpp"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	cout << "Esto es el main, no tiene ninguna funcionalidad." << endl;
+	
+	return 0;
+}
