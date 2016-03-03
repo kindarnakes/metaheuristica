2ecfad27b5724beef71fdb65e994199cec38c027
carlosccb <carlos.cb.94@gmail.com>
2016-03-03 17:26:18 +0100

    Creada un clase interfaz para CLI que carga instancias de un fichero y se ocupa de todas las entradas de usuario

---
+++ Practica 1/lib/Interfaz.hpp
@@ -0,0 +1,75 @@
+#ifndef __INTERFAZ_HPP__
+#define __INTERFAZ_HPP__
+
+#include <iostream>
+#include <vector>
+
+#include "Miscelanea.hpp"
+#include "Instance.hpp"
+#include "../KP/InstanceKP.hpp"
+#include "../TSP/InstanceTSP.hpp"
+
+class Interfaz {
+	private:
+		int opt;
+		std::string fileName;
+
+	public:
+		Interfaz() {}
+		~Interfaz() {}
+
+		int option_menu() {
+			do {
+				std::cout << "\n\n";
+				std::cout << "¿Que problema desea ejecutar?: " << std::endl;
+				std::cout << "  1. TSP" << std::endl;
+				std::cout << "  2. KP" << std::endl;
+				std::cout << "  0. Exit" << std::endl;
+
+				std::cin >> opt;
+			} while(0 > opt || opt > 2);
+
+			return opt;
+		}
+
+		int getOpt() {return opt;}
+
+		std::vector<problem_element> read_instance() {
+			Instance *inst;
+			bool ret = false;
+			std::vector<problem_element> inst_v;
+
+			while(not ret) {
+				std::string fileName;
+				std::cout << "Introduzca el nombre del fichero: ";
+				std::cin >> fileName;
+
+				if(opt == 1) {
+					fileName =  "TSP/" + fileName;
+					inst = new InstanceTSP(fileName);
+					ret = inst->load_instance(inst_v);
+				} else if(opt == 2) {
+					fileName =  "KP/" + fileName;
+					inst = new InstanceKP(fileName);
+
+					int inst_num;
+					std::cout << "¿Que numero de instancia desea cargar?: ";
+					std::cin >> inst_num;
+					ret = inst->load_instance(inst_v, inst_num);
+				}
+
+				if(not ret)
+					std::cerr << "ERROR: El fichero no se pudo abrir.\n";
+			}
+
+			return inst_v;
+		}
+
+		void print_instance(std::vector<problem_element> &inst_v) {
+				std::cout << "\nInstancia :";// << instance.inst_num << " de " << instance.fileName << endl;
+				for(unsigned int i = 0; i < inst_v.size(); i++)
+					std::cout << inst_v[i].id << ": \n  a: " << inst_v[i].a << "\n  b: " << inst_v[i].b << std::endl;
+		}
+};
+
+#endif
