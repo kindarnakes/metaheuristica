5690a3d164862ea4b7beb72e07057d74fbe7489b
carlosccb <carlos.cb.94@gmail.com>
2016-03-03 17:26:47 +0100

    Adaptado a la Interfaz

--- Practica 1/main.cpp
+++ Practica 1/main.cpp
@@ -7,6 +7,7 @@
 #include "lib/Timer.h"
 */
 
+#include "lib/Instance.hpp"
 #include "KP/SolucionMochila.hpp"
 #include "TSP/SolucionViajante.hpp"
 
@@ -15,11 +16,17 @@ using namespace std;
 int main(int argc, char **argv) {
 	Interfaz cli;
 
-	do {
-		cli.option_menu();
-	} while(cli.getOpt());
+	//Siempre se ejcuta hasta que el usuario quiera salir
+	while(true) {
+		if(not cli.option_menu())
+			break;
 
-	cout << "Esto es el main, no tiene ninguna funcionalidad." << endl;
+		//Se ejecuta hasta que se carga una instancia de un fichero valido
+		vector<problem_element> inst_v = cli.read_instance();
+
+		//Aqui iria el codigo que hace algo con una instancia
+		cli.print_instance(inst_v);
+	}
 
 	return 0;
 }
