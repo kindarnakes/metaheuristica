9d34bd0f52da503c3e9755d7a4e1ba242d550141
carlosccb <carlos.cb.94@gmail.com>
2016-03-05 23:20:55 +0100

    Ha pasado algo raro con la indentacion xD

--- Practica 1/TSP/SolGeneratorViajante.hpp
+++ Practica 1/TSP/SolGeneratorViajante.hpp
@@ -26,7 +26,7 @@ class SolGeneratorViajante{
 
 		SolucionViajante randomSolutionGenerator(unsigned int objetos){
 			
-            SolucionViajante solucion(objetos);
+			SolucionViajante solucion(objetos);
 
 			for(unsigned int i = 0; i < objetos; i++)
 				solucion.setSolucion(i, i);
@@ -34,7 +34,7 @@ class SolGeneratorViajante{
 			for(unsigned int i = 0; i < objetos; i++)
 				 solucion.setSolucion(rand()%objetos, i);
 
-            return solucion;
+			return solucion;
 		}
 };
 
