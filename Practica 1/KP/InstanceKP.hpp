6554aead97f8f6d179a6b66e13866c6b9b556808
carlosccb <carlos.cb.94@gmail.com>
2016-03-07 14:52:16 +0100

    No compila, modificaciones subidas para no perderlas

--- Practica 1/KP/InstanceKP.hpp
+++ Practica 1/KP/InstanceKP.hpp
@@ -13,7 +13,11 @@
 #include "SolucionMochila.hpp"
 
 class InstanceKP : public Instance {
+	private:
+		unsigned int _capacity;
+
 	public:
+		InstanceKP() {}
 		//Valores del constructor adaptados a los archivos csv de esta practica
 		InstanceKP(std::string file_name, int header_lines=5, int length_line=1, char separator=',', int end_lines=2) 
 			: Instance(file_name, header_lines, length_line, separator, end_lines) 
@@ -24,6 +28,50 @@ class InstanceKP : public Instance {
 			if(_file.is_open())
 				_file.close();
 		}
+
+		bool header() {
+			if(not _file.is_open()) {
+				std::cerr << "El fichero esta cerrado." << std::endl;
+				return false;
+			}
+
+			for(unsigned int i = 0; i < _header_lines; i++) {
+				if(i == _length_line) {
+					std::string line;
+					getline(_file, line);
+					const std::string c_line = line;
+					std::regex rgx(".* (\\d+) *$");
+					std::smatch match;
+
+					if (std::regex_search(c_line.begin(), c_line.end(), match, rgx))
+						_instance_length = std::stoi(match[1]);
+					else {
+						std::cerr << "No se encontro longitud de la instancia en la cabecera del fichero." << std::endl;
+						exit(-1);
+					}
+				}
+				//En el caso de que sea la linea que contiene la capacidad de la mochila
+				else if (i == _length_line + 1) {
+					std::string line;
+					getline(_file, line);
+					const std::string c_line = line;
+					std::regex rgx(".* (\\d+) *$");
+					std::smatch match;
+
+					if (std::regex_search(c_line.begin(), c_line.end(), match, rgx))
+						_capacity = std::stoi(match[1]);
+					else {
+						std::cerr << "No se encontro longitud de la instancia en la cabecera del fichero." << std::endl;
+						exit(-1);
+					}
+
+				}
+				else
+					_file.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+			}
+		}
+
+		unsigned int  getCapacity() {return _capacity;}
 
 
 		//Funcion que devuelve la bondad de una solucion
