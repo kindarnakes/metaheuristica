8000e550b58b797de36010d85d85a4cecf0d271c
i22bomui <i22bomui@uco.es>
2016-03-22 14:04:31 +0100

    Añadida Generalizacion clase neighborOperator

---
+++ Practica 2/neighborOperator.hpp
@@ -0,0 +1,47 @@
+#ifndef		__NEIGHBOR_OPERATOR__
+#define		__NEIGHBOR_OPERATOR__
+
+
+#include <vector>
+
+using namespace std;
+
+
+
+/* De momento lo estoy haciendo todo de forma general para ver como hacerlo
+
+
+		Quizas podamos implementar en esta clase todo como metodos, pero no 
+		se si a lo mejor seria mas coñazo
+
+*/
+
+
+class neighborOperator{
+
+
+	private:
+
+
+
+	public:
+
+		//Constructor vacio
+		neighborOperator(){};
+
+
+		//Metodo que genera un vecino de una solucion, teniendo en cuenta unos parametros
+		Solution generateNeighbor(Solucion &initialSolution, const int &parametros){
+
+			
+
+				//Ni idea de que hay que hacer con los parametros o de cuales son
+				//Pero el resto es solo implementar la funcionalidad de ese operador
+				
+
+
+		}
+
+};
+
+#endif
